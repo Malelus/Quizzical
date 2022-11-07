bbc0077953a586322e14924db60e67821ed61939
Mateusz Narowski <matin1608@gmail.com>
2022-11-07 04:06:31 +0100

    Footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,35 @@
 import { useState } from 'react';
 
+import HidingSidebarOnBlur from './HidingSidebarOnBlur';
+
 const Footer = () => {
   const [showFooter, setShowFooter] = useState(false);
 
   return (
     <>
-      <footer className={`footer ${showFooter ? 'footer--visible' : ''}`} onBlur={() => setShowFooter(false)}>
-        <a href='https://github.mndev.eu' target='_blank' className='btn btn--icon'>
+      <HidingSidebarOnBlur showFooter={showFooter} setShowFooter={setShowFooter}>
+        <a
+          href='https://github.mndev.eu'
+          target='_blank'
+          className='btn btn--icon'
+          onClick={() => setShowFooter(false)}
+        >
           Author <i className='fa-brands fa-github' />
         </a>
 
-        <a href='https://github.mndev.eu/Quizzical' target='_blank' className='btn btn--icon'>
+        <a
+          href='https://github.mndev.eu/Quizzical'
+          target='_blank'
+          className='btn btn--icon'
+          onClick={() => setShowFooter(false)}
+        >
           Project <i className='fa-brands fa-github' />
         </a>
 
         <button className='btn btn--icon footer__toggle' onClick={() => setShowFooter((prev) => !prev)}>
           <i className='fa-brands fa-github' />
         </button>
-      </footer>
+      </HidingSidebarOnBlur>
 
       <a href='https://opentdb.com/' target='_blank' className='footer__source'>
         API <i className='fa-solid fa-globe' />
